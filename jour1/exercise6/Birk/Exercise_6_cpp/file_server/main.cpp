dc5b802bf066d17f5154503bd1749cb6682f6769
mbirko <github@mbirk.dk>
2022-02-21 11:56:28 +0100

    BIrk started to analyse server

--- jour1/exercise6/Birk/Exercise_6_cpp/file_server/main.cpp
+++ jour1/exercise6/Birk/Exercise_6_cpp/file_server/main.cpp
@@ -15,6 +15,12 @@
 
 using namespace std;
 
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
 void sendFile(string fileName, long fileSize, int outToClient);
 
 /**
@@ -30,19 +36,82 @@ void sendFile(string fileName, long fileSize, int outToClient);
  *
  */
 int main(int argc, char *argv[])
-{
-    // argument filtering 
-    if (argc < 2){printf("need address and port as input\n"); return 1;}
-    
+{   
+    //variebels for:
+    //// sock file descriptor 
+    //// new sock file desctriptor
+    //// port number that the server accepts data from
+    int sockfd, newsockfd, portno;
+    // clilen stores the size of the address of the client.
     socklen_t clilen;
-	char buffer[BUFSIZE];
-	char bufferTx[BUFSIZE];
-	struct sockaddr_in serv_addr, cli_addr;
-	int n;
+    // rx vuffer from bytes read from socket connection
+    char buffer[256];
+    // sockadd_in hold an internet address.
+    //// serv_addr holds the server address.
+    //// cli_addr holds the client address.
+    struct sockaddr_in serv_addr, cli_addr;
+    // n is used to hold the return value from read and write
+    int n;
+    // This checks that the correct arguments was passed. 
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+    // socket() is a system call
+    //// The first argument determins if its unix or internet domain
+    //// the second argument defines the type of socket.
+    ////// Two options, stream and datagram,
+    ////// the stream continuesly gets data like reading form a file
+    //// the thired argument is the protocal. 
+    ////// 0 lets the compiler chose the most appropiate
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // Checks if an error occured in opening the socket.
+    if (sockfd < 0) 
+       error("ERROR opening socket");
+    // bzero sets elemets in a buffer to zero. Takes address and legth. 
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    // saves the second cmd argument, the portnumber, as a int to portno.
+    portno = atoi(argv[1]);
+    // setting up the struct server addreess
+    //// the first element famility is set to internet
+    serv_addr.sin_family = AF_INET;
+    //// the address of the host
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    //// port number is converted to network byte order, big ediram
+    serv_addr.sin_port = htons(portno);
+    // bind bindes a socket to address.
+    //// first argument is the socket file
+    //// secod argument is the address of type sockaddr
+    //// Then the size of of the address
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+            sizeof(serv_addr)) < 0) 
+        {
+            error("ERROR on binding");
+        }
 
-    // TO DO Your own code
-}
+    // listen() allows the process to lisen for connections 
+    //// takes the socket it should lisen on
+    //// and the number that can be in cue while one connection.
+    listen(sockfd,5);
+    //the client address lengt is saved in clilen.
+    clilen = sizeof(cli_addr);
+    // 
+    newsockfd = accept(sockfd, 
+                (struct sockaddr *) &cli_addr, 
+                &clilen);
 
+    if (newsockfd < 0) 
+         error("ERROR on accept");
+
+    bzero(buffer,256);
+    n = read(newsockfd,buffer,255);
+    if (n < 0) error("ERROR reading from socket");
+    printf("Here is the message: %s\n",buffer);
+    n = write(newsockfd,"I got your message",18);
+    if (n < 0) error("ERROR writing to socket");
+    close(newsockfd);
+    close(sockfd);
+    return 0; 
 /**
  * Sender filen som har navnet fileName til klienten
  *
