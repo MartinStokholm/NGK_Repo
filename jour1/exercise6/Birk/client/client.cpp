de1046ca5831fdcba80d3c5d621441a52b64f75f
Birk <git@mbirk.dk>
2022-03-05 17:03:18 +0100

    Birks string clinet still buged

--- jour1/exercise6/Birk/client/client.cpp
+++ jour1/exercise6/Birk/client/client.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <arpa/inet.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
@@ -19,18 +20,75 @@ void error(const char *msg)
     perror(msg);
     exit(0);
 }
+int set_up_client_socket()
+{
+
+    // setup: 
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd != -1)
+	{
+		printf("socket for client created");
+		return sockfd;
+	}
+	
+	printf("ERROR on creating socket\n");		
+	exit(0);
+}
 
 
+struct sockaddr_in define_server_socket(const char * hostname,int portno )
+{
+	struct sockaddr_in server_address; 
+//	struct hostent *server;
+//
+//	The IP is the name of the server! 
+//	server = gethostbyname(hostname);
+//	if (server == NULL)
+//	{
+//		error("ERROR: failed to get host by name");
+//		exit(0);	    
+//	}    
+
+	bzero((char*) &server_address, sizeof(server_address));
+
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(portno);
+	if(inet_pton(AF_INET, hostname, &server_address.sin_addr) <= 0)
+	{
+		printf("invalid address / not supported\n");
+	}
+	printf("Server address defined\n");
+	return server_address;
+}
+void connect_client_to_server(	int sockfd, 
+								struct sockaddr * server_address, 
+								int size_of_server_address)
+{
+
+	if (connect(sockfd, server_address, size_of_server_address) < 0)
+	{
+		error("ERROR Connecting");
+	}
+
+	printf("connected to server!\n");
+	return;
+}
 void write_message_to_server(int sockfd, char * buffer, int buf_len)
 {
 	int n = write(sockfd, buffer, buf_len);
-	if (n < 0) 
+	if (n < 0 || n != buf_len) 
 		error("ERROR writing to socket");  
 }
 
-void read_message_from_server(int sockfd, char *msg_from_server, int &msg_len)
+int read_message_from_server(int sockfd, char *msg_from_server, int msg_max_len)
 {
-
+	int bytes_read = read(sockfd, msg_from_server, msg_max_len);
+	if (bytes_read < 0) 
+	{
+		printf("error reading\n");
+		exit(0);
+	}
+	return bytes_read; 
 }
 
 
@@ -46,29 +104,13 @@ int main(int argc, char *argv[])
     int portno = atoi(argv[2]);
     const char *hostname = argv[1];
 
-    // setup: 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    struct hostent *server;
-	// The IP is the name of the server! 
-    server = gethostbyname(hostname);
-    if (server == NULL)
-	{
-		error("ERROR: failed to get host by name");
-		exit(0);	    
-	}    
-	bzero((char*) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	// void bcopy(char *s1, char *s2, int length); 
-	bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
-	serv_addr.sin_port = htons(portno);
-
-	if (connect(sockfd,&serv_addr,sizeof(serv_addr)) < 0)
-		error("ERROR Connecting");
-
+	int sockfd = set_up_client_socket();
+	serv_addr = define_server_socket(hostname, portno);
+	connect_client_to_server(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr));
 
     // user input variebels
     char *answer; 
-    int msg_len;
+    int msg_max_len = 255;
     char msg_from_server[BUFFER_LENGTH];
 
     do { //
@@ -77,9 +119,9 @@ int main(int argc, char *argv[])
         fgets(buffer, BUFFER_LENGTH, stdin);
 
         write_message_to_server(sockfd, buffer, strlen(buffer));
-        read_message_from_server(sockfd, msg_from_server, &msg_len);
-        
-        printf("Do you wish to send another message?");
+        read_message_from_server(sockfd, msg_from_server, msg_max_len);
+		printf(msg_from_server);  
+        printf("Do you wish to send another message?(y/n): ");
         fgets(answer,2,stdin);
     } while (answer[0] != 'n');
     
